6c71f5188bb0e298e9c7690beed904bf782e6e73
fickkmdev <fickkm.dev@gmail.com>
2022-08-20 13:09:55 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,11 +2,25 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [str, setStr] = React.useState<string>('init value');
+  const [count, setCount] = React.useState(0);
+  const ins = () =>{
+    setCount((c)=>c+1)
+    setCount((c)=>c+1)
+    setCount((c)=>c+1)
+    setCount((c)=>c+1)
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <p>hello react 101</p>
+    <div style={{display:'flex', justifyContent:'center', alignItem:'center', flexDirection:'column'}}>
+      <div>{str}</div>
+      <input 
+        value = {str}
+        onChange={(e)=>setStr(e.target.value)}
+      />
+      <div style={{marginTop:'30px'}}>{count}</div>
+      <button
+        onClick={ins}
+      >increase</button>
     </div>
   );
 }
