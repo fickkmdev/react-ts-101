83cb054c58d358a983659a190a3a612575d81306
fickkmdev <fickkm.dev@gmail.com>
2022-08-21 11:37:16 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,45 +1,32 @@
 import * as React from 'react';
-import CountComponent from './CountComponent';
-import InputComponent from './InputComponent';
-import './style.css';
 
-const Child = React.forwardRef<unknown, {label: string}>((props,ref) => {
-  const focus = React.useRef<number>()
-  const inputRef1 = React.useRef<HTMLInputElement>();
-  const inputRef2 = React.useRef<HTMLInputElement>();
+const contextValue = React.createContext(undefined);
 
-  React.useImperativeHandle(ref, () => ({
-    focus: ()=>{
-      inputRef2.current.focus();
-      focus.current=2;
-    },
-    unfocus: ()=>{
-      if(focus.current===2){
-        focus.current=1;
-        inputRef1.current.focus()
-      }else{
-        focus.current = 2;
-        inputRef2.current.focus()
-      }
-    }
-  }), []);
+function Child1() {
   return (
-  <div>
-    <input ref={inputRef1}/>
-    <input ref={inputRef2}/>    
-  </div>);
-});
+    <contextValue.Consumer>
+      {(props) => {
+        return <div>child1 context: {props}</div>;
+      }}
+    </contextValue.Consumer>
+  );
+}
+
+function Child2() {
+  const props = React.useContext(contextValue);
+  return <div>child2 context: {props}</div>;
+}
 
 export default function App() {
-  const [value1, setValue1] = React.useState<string>('');
-  const [value2, setValue2] = React.useState<string>('');
-  const childRef = React.useRef()
+  const [value, setValue] = React.useState<string>();
 
   return (
-    <div>
-      <Child ref={childRef} label={''}/>
-      <button onClick={()=>{childRef.current.focus()}}>focus</button>
-      <button onClick={()=>{childRef.current.unfocus()}}>unfocus</button>
-    </div>
+    <contextValue.Provider value={value}>
+      <div>
+        <Child1/>
+        <Child2/>
+        <input onChange={(e) => setValue(e.target.value)} />
+      </div>
+    </contextValue.Provider>
   );
 }
