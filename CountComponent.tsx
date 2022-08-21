fb9fa13c02cc7f33a8eb058f98cc622ec2a591dd
fickkmdev <fickkm.dev@gmail.com>
2022-08-21 09:27:34 +0700

    1 file modified

--- CountComponent.tsx
+++ CountComponent.tsx
@@ -3,10 +3,8 @@ import * as React from 'react'
 export default function CountComponent(props){
   return (
     <div>
-      <div style={{marginTop:'30px'}}>{props.count}</div>
-      <button
-        onClick={()=>props.setCount((c)=>c+1)}
-      >increase</button>
+      <input/>
+      <button></button>
     </div>
   )
 }
